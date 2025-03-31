249b8f80c9f87b9b274587f8f3af6462de85cc6f
Michael Riha <michael.riha@bonrepublic.com>
2025-03-31 13:17:45 +0200

    renamed lib with underscore (_) instead of hypen (-)
    >  In Python, the use of the hyphen (-) character in package names is discouraged

---
+++ libs/lib_internal_ref/src/lib_internal_ref/some_module.py
@@ -0,0 +1,6 @@
+def some_function() -> str:
+    """
+    This is a simple function that prints a message.
+    """
+    print("Hello from lib_internal_ref's some_function!")
+    return "Hello from lib_internal_ref's some_function!"
